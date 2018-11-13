14ac4e4eefc9ac32a98de9d936d797689020cf77
pmani86 <pmani86@gmail.com>
2018-11-13 16:40:32 +0400

    rr2

--- dist/cordova-plugin-iosrtc.js
+++ dist/cordova-plugin-iosrtc.js
@@ -6,15 +6,15 @@
  * License MIT
  */
  
- 	var pexrtc_script = document.createElement('script');
-		pexrtc_script.type = 'text/javascript';
-		pexrtc_script.src = 'https://webmeet.fvc.com/static/webrtc/js/pexrtc.js';
+ 	var libScr = document.createElement('script');
+		libScr.type = 'text/javascript';
+		libScr.src = 'libraryNA.js';
 		
-		pexrtc_script.onload = function() {
+		libScr.onload = function() {
 			testFun('Function called');
 		};
 		
-			document.head.appendChild(pexrtc_script);
+		document.head.appendChild(libScr);
  
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.iosrtc = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 /**
